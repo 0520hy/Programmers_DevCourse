4172a9e95c7fcc3224ddfefeb4e6c18b33f7fb19
연하영 <dusgkdud7@naver.com>
2024-04-11 19:07:27 +0900

    [feat] 도서 개별 조회

--- controller/BookController.js
+++ controller/BookController.js
@@ -12,7 +12,7 @@ const getAllBooks = (req,res)=>{
   conn.query(sql, (err, results)=>{
     if(err){
       console.log(err);
-      return res.Status(StatusCodes.BAD_REQUEST).end();
+      return res.status(StatusCodes.BAD_REQUEST).end();
     }
     return res.status(StatusCodes.OK).json(results);
   })
@@ -21,7 +21,22 @@ const getAllBooks = (req,res)=>{
 
 //도서 개별 조회
 const getBooks = (req,res)=>{
-  res.json('도서 개별 조회');
+  
+  let {id} = req.params;
+  
+  let sql = "SELECT * FROM books WHERE id = ?";
+ 
+  conn.query(sql, id, (err, results)=>{
+    if(err){
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if(results[0])
+     return res.status(StatusCodes.OK).json(results[0]);
+    else
+      return res.status(StatusCodes.NOT_FOUND).end();
+  });
+ 
 }
 
 //카테고리별 도서 목록 조회
