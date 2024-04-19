ac8e26fc07affb4d681134e488a8be3b95bef7f0
연하영 <dusgkdud7@naver.com>
2024-04-20 00:35:49 +0900

    [feat] 주문하기 api 비동기 처리

--- mariadb.js
+++ mariadb.js
@@ -1,13 +1,16 @@
 // mysql 모듈
-const mariadb = require('mysql2');
+const mariadb = require('mysql2/promise');
 
 //DB와 연결통로 생성
-const connection = mariadb.createConnection({
-  host: 'localhost',
-  user : 'root',
-  password : 'root',
-  database : 'BookStore',
-  dateStrings : true
+const connection = async()=> {
+   const conn = await mariadb.createConnection({
+      host: 'localhost',
+      user : 'root',
+      password : 'root',
+      database : 'BookStore',
+      dateStrings : true
 });
+  return conn;  
+}
 
 module.exports = connection;
