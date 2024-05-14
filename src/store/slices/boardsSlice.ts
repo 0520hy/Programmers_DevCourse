49dc1282f130d9e0f37be2ee2392af25321df4eb
연하영 <dusgkdud7@naver.com>
2024-05-15 01:28:16 +0900

    Board 데이터 및 타입 생성

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -2,7 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState ={
     modal: false,
-    boardArray: []
+    boardArray: [
+        {
+            boardId: 'board-0',
+            boardName: '첫 번째 게시물',
+            list: [
+                {
+                    listId: 'List-0',
+                    ListName: "List 1"
+                }
+            ]
+        }
+    ]
 }
 
 const boardsSlice = createSlice({
