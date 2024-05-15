3f812abbe703acf06be09d2103d8b1bdb3d786d4
연하영 <dusgkdud7@naver.com>
2024-05-15 22:01:33 +0900

    redux hooks 생성

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -1,15 +1,48 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { IBoard } from "../../types";
 
-const initialState ={
-    modal: false,
+type TBoardState = {
+    modalActive: boolean;
+    boardArray: IBoard[];
+}
+
+const initialState : TBoardState ={
+    modalActive: false,
     boardArray: [
         {
             boardId: 'board-0',
             boardName: '첫 번째 게시물',
-            list: [
+            lists: [
                 {
                     listId: 'List-0',
-                    ListName: "List 1"
+                    listName: "List 1",
+                    tasks: [
+                        {
+                            taskId: 'task-0',
+                            taskName: 'Task 1',
+                            taskDescription: 'Description',
+                            taskOwner: 'yeon'
+                        },
+                        {
+                            taskId: 'task-1',
+                            taskName: 'Task 2',
+                            taskDescription: 'Description',
+                            taskOwner: 'yeon'
+                        },
+                    ]
+                },
+                {
+                    listId: 'List-1',
+                    listName: "List 2",
+                    tasks: [
+                        {
+                            taskId: 'task-3',
+                            taskName: 'Task 3',
+                            taskDescription: 'Description',
+                            taskOwner: 'yeon'
+                        },
+                       
+                    ]
                 }
             ]
         }
