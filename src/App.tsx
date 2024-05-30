816710263611ae1940d50673bcf7de88a6e75da4
연하영 <dusgkdud7@naver.com>
2024-05-31 02:13:36 +0900

    themeSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Home from './pages/Home';
 import Layout from './components/layout/Layout';
+import { GlobalStyle } from './style/global';
+import { ThemeProvider } from 'styled-components';
+import {  getTheme } from './style/theme';
+import ThemeSwitcher from './components/header/ThemeSwitcher';
+import { BookStoreThemeProvider, ThemeContext } from './context/themeContext';
 
 function App() {
+
   return (
-    // <Layout children={<Home/>} />
-    <Layout>
-      <Home />
-    </Layout>
+    <BookStoreThemeProvider>
+   
+      <ThemeSwitcher/>
+      <Layout>
+        <Home />
+      </Layout>
+    
+    </BookStoreThemeProvider>
   );
 }
 
