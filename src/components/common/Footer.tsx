050b83cd6acf08e5f1e17b984f4a3aed04fc9c49
연하영 <dusgkdud7@naver.com>
2024-05-30 18:56:45 +0900

    git test

---
+++ src/components/common/Footer.tsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <>
+        <hr/>
+        <div>
+            copyright(c), 2024, bookstore
+        </div>
+        </>
+    )
+}
+
+export default Footer;
