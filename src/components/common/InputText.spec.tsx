50c4f7fa40a7e2a85d830d8a362f1ff4226719ac
연하영 <dusgkdud7@naver.com>
2024-06-01 18:38:43 +0900

    Input 컴포넌트

---
+++ src/components/common/InputText.spec.tsx
@@ -0,0 +1,28 @@
+import { render, screen} from "@testing-library/react"
+import InputText from "./InputText"
+import { BookStoreThemeProvider } from "../../context/themeContext";
+import React from "react";
+
+describe("InputText 컴포넌트 테스트",()=>{
+    it('렌더 확인', ()=> {
+        // 1. 렌더
+        render(
+            <BookStoreThemeProvider>
+        <InputText placeholder="여기에 입력"/>
+        </BookStoreThemeProvider>
+    );
+        // 2. 확인
+        expect(screen.getByPlaceholderText("여기에 입력")).toBeInTheDocument();
+        // 화면상에 제목이라는 text를 가져와서 있는지 확인
+    })
+    it('forwardRef 테스트', ()=> {
+        const ref = React.createRef<HTMLInputElement>();
+         render(
+            <BookStoreThemeProvider>
+        <InputText placeholder="여기에 입력" ref={ref}/>
+        </BookStoreThemeProvider>
+         )
+        expect(ref.current).toBeInstanceOf(HTMLInputElement)
+        
+    })
+})
