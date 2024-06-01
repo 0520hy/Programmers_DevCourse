9dc27d2da425636d004057bc076d6fc9f4c08b02
연하영 <dusgkdud7@naver.com>
2024-06-02 01:33:47 +0900

    model 정의

---
+++ src/models/order.model.ts
@@ -0,0 +1,10 @@
+export interface Orders {
+    id: number;
+    createdat: string;
+    address: string;
+    receiver: string;
+    contact: string;
+    bookTitle: string;
+    totalQuantity: number;
+    totalPrice: number;
+}
