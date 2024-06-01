996156a4acf7e7b0be56ce97e20bc7400dd22f9a
연하영 <dusgkdud7@naver.com>
2024-06-02 00:53:44 +0900

    route 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,28 @@
-
-import Home from './pages/Home';
 import Layout from './components/layout/Layout';
-import ThemeSwitcher from './components/header/ThemeSwitcher';
+import Home from "./pages/Home";
 import { BookStoreThemeProvider } from './context/themeContext';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from './components/common/Error';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:   <Layout><Home/></Layout>,
+        errorElement: <Error/>
+    },
+    {
+        path: "/books",
+        element: <Layout>
+          <div>도서목록</div>
+        </Layout>
+    }
+])
 
 function App() {
 
   return (
     <BookStoreThemeProvider>
-      <Layout>
-        <Home />
-      </Layout>
-    
+        <RouterProvider router={router}/>
     </BookStoreThemeProvider>
   );
 }
