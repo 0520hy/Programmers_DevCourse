44126d95ee4339a86aaeb3ba806156492d2b4980
연하영 <dusgkdud7@naver.com>
2024-06-16 21:07:23 +0900

    craco 적용

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { Children } from "react";
+import React from "react";
 import Footer from "../common/Footer";
 import Header from "../common/Header";
 import styled from "styled-components";
@@ -19,7 +19,7 @@ function Layout({children}:LayoutProps){
 
 
 const LayoutStyle = styled.main`
-     width: 100%;
+    width: 100%;
     margin: 0 auto;
     max-width: ${({theme}) => theme.layout.width.large};
     padding: 20px 0%;
